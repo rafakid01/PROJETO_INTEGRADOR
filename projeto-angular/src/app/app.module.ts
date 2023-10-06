e368deab8253648a7fc10dcab803721161155faa
Rafael Silva <rs7951006@gmail.com>
2023-10-06 17:57:08 -0300

    navbar 1 version

--- projeto-angular/src/app/app.module.ts
+++ projeto-angular/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { HowWorksComponent } from './components/how-works/how-works.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    HowWorksComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
