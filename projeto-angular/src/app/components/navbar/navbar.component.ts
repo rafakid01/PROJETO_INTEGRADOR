3f2b43d5d5942ad2484c552928c09298470aec98
rafaki01 <rs7951006@gmail.com>
2023-10-08 21:25:39 -0300

    falta arrumar a mudança de cores

--- projeto-angular/src/app/components/navbar/navbar.component.ts
+++ projeto-angular/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  styles: [],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  constructor(private renderer: Renderer2) {}
 
+  changeColorTheme() {}
+
+  ngOnInit(): void {}
 }
