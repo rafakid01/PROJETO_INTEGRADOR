4174d229cda858b9f9584f804cd1ce764767f767
Rafael Silva <rs7951006@gmail.com>
2023-10-10 20:52:09 -0300

    falta mudar a classe do index principal

--- projeto-angular/src/app/services/change-theme.service.ts
+++ projeto-angular/src/app/services/change-theme.service.ts
@@ -4,7 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ChangeThemeService {
+  theme_final: any;
   constructor() {}
 
-  getTheme(theme: String) {}
+  getTheme(theme: String) {
+    this.theme_final = theme;
+  }
+
+  importTheme() {
+    return this.theme_final;
+  }
 }
