6fdc94906829ad6a0e1f54b6165e72a6c337f805
Rafael Silva <rs7951006@gmail.com>
2023-10-12 21:30:45 -0300

    ble ble ble

--- projeto-angular/src/app/app.component.ts
+++ projeto-angular/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { LocalStorageService } from './services/local-storage.service';
+
+declare function atualizarTemaCores(className: string): any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-angular';
+  theme_class: any;
+
+  constructor(
+    private localStorage: LocalStorageService,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit(): void {
+    this.theme_class = this.localStorage.getItem('theme');
+    atualizarTemaCores(this.theme_class);
+
+    this.renderer.setProperty(document, 'class', this.theme_class);
+
+    // if (this.theme_class == undefined) {
+    //   this.theme_class = this.localStorage.getItem('theme');
+    // } else {
+    //   this.theme_class = this.ThemeService.importTheme();
+    // }
+  }
 }
