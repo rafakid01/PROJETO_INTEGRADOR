2e365d07e7e80fed0f2fc0b3f5cfeda03b28dc55
Rafael Silva <rs7951006@gmail.com>
2023-10-17 18:24:29 -0300

    mudança de icone e input login

--- projeto-angular/src/app/components/login/login.component.ts
+++ projeto-angular/src/app/components/login/login.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private Renderer: Renderer2) {}
+  typeInputPass: string = 'password';
+  typeEye: string = 'bi bi-eye-fill';
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {}
+
+  changeVisibility() {
+    this.typeInputPass == 'password'
+      ? (this.typeInputPass = 'text')
+      : (this.typeInputPass = 'password');
+
+    this.typeEye == 'bi bi-eye-fill'
+      ? (this.typeEye = 'bi bi-eye-slash-fill')
+      : (this.typeEye = 'bi bi-eye-fill');
+  }
 }
